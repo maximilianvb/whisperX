9b97252bc1dee3c56815796c33afe8a46de9d0e3
Maximilian von Bonsdorff <62935939+maximilianvb@users.noreply.github.com>
2024-09-01 17:58:15 +0200

    WIP exploring

--- whisperx/transcribe.py
+++ whisperx/transcribe.py
@@ -18,6 +18,7 @@ def cli():
     # fmt: off
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument("audio", nargs="+", type=str, help="audio file(s) to transcribe")
+    parser.add_argument("--compound_words", type=str, default="", help="Add words separated by space that you want to not be compounded.")
     parser.add_argument("--model", default="small", help="name of the Whisper model to use")
     parser.add_argument("--model_dir", type=str, default=None, help="the path to save model files; uses ~/.cache/whisper by default")
     parser.add_argument("--device", default="cuda" if torch.cuda.is_available() else "cpu", help="device to use for PyTorch inference")
